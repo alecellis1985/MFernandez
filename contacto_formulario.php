add6a43f2a29d6349e15fa0ccc10bbd3d7c33cbf
MSI\Alexander <alecellis1985@gmail.com>
2017-05-08 18:15:49 -0300

    fixed contact form index and google maps issue

--- contacto_formulario.php
+++ contacto_formulario.php
@@ -1,61 +1,44 @@
 <?php
-if($_POST)
-{
-	$to_Email   	= "ventas@ruffinogroup.com.uy, ventas@emin.com.uy, info@emin.com.uy";
-	$subject        = 'Mensaje desde sitio web eMin';
 
+$user_Name = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
+$user_Email = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
+$user_Message = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
+$tel = $_POST["tel"];
 
-	//chqueo si esl formulario se llamo por ajax
-    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')
-    {
+if (isset($user_Name) && !empty($user_Name) &&
+        isset($user_Email) && !empty($user_Email) &&
+        isset($user_Message) && !empty($user_Message) &&
+        isset($tel) && !empty($tel)) {
+  if (!filter_var($user_Email, FILTER_VALIDATE_EMAIL)) {
+    $output = json_encode(array('type' => 'error', 'text' => 'Ingrese un email válido'));
+    die($output);
+  }
 
-		$output = json_encode(
-		array(
-			'type'=>'error',
-			'text' => 'Error al procesar formulario de contacto'
-        ));
+  $messageBody = "";
+  $messageBody .= "<p>Nombre: " . $user_Name . "</p>" . "\r\n";
+  $messageBody .= "<p>Telefono: " . $tel . "</p>" . "\r\n";
+  $messageBody .= "<br>" . "\r\n";
+  $messageBody .= "<p>Email: " . $user_Email . "</p>" . "\r\n";
+  $messageBody .= "<br>" . "\r\n";
+  $messageBody .= "<p>Mensaje: " . $user_Message . "</p>" . "\r\n";
+  $messageBody .= "<br>" . "\r\n";
+  $messageBody = strip_tags($messageBody);
 
-		die($output);
-    }
+  $to = "ventas@ruffinogroup.com.uy, ventas@emin.com.uy, info@emin.com.uy";
+  $subject = "Mensaje desde sitio web eMin";
+  $headers = "From:" . $user_Email . "\r\n";
+  $sent = mail($to, $subject, $messageBody, $headers);
 
-	if(!isset($_POST["userName"]) || !isset($_POST["userEmail"]) || !isset($_POST["userMessage"]))
-	{
-		$output = json_encode(
-            array(
-                'type'=>'error',
-                'text' => 'Todos los campos son obligatorios'
-            ));
-		die($output);
-	}
-
-	$user_Name        = filter_var($_POST["userName"], FILTER_SANITIZE_STRING);
-	$user_Email       = filter_var($_POST["userEmail"], FILTER_SANITIZE_EMAIL);
-	$user_Message     = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
-	$tel     =  $_POST["tel"];
-
-	$user_Message = str_replace("\&#39;", "'", $user_Message);
-	$user_Message = str_replace("&#39;", "'", $user_Message);
-
-	if(!filter_var($user_Email, FILTER_VALIDATE_EMAIL))
-	{
-		$output = json_encode(array('type'=>'error', 'text' => 'Ingrese un email válido'));
-		die($output);
-	}
-
-	//Enviar correo.
-	$headers = 'From: '.$user_Email.'' . "\r\n" .
-	'Reply-To: '.$user_Email.'' . "\r\n" .
-	'X-Mailer: PHP/' . phpversion();
-
-	$sentMail = @mail($to_Email, $subject, 'Nombre: '.$user_Name. "\r\n\n" .'Email: '.$user_Email. "\r\n\n".'Tel.: '.$tel. "\r\n\n" .'Mensaje:'. "\n" .$user_Message, $headers);
-
-	if(!$sentMail)
-	{
-		$output = json_encode(array('type'=>'error', 'text' => 'Problemas con el servidor de correo, intente enviar el mensaje más tarde'));
-		die($output);
-	}else{
-		$output = json_encode(array('type'=>'message', 'text' => 'Tu mensaje se envió correctamente '.$user_Name .', gracias por comunicarte'));
-		die($output);
-	}
+  if ($sent) {
+    die(json_encode(array('type' => 'message', 'text' => 'Tu mensaje se envió correctamente ' . $user_Name . ', gracias por comunicarte')));
+  } else {
+    error_log($subject . "\r\n\n mensaje:" . $messageBody . "\r\n\n From" . $user_Email . "\r\n\nErrorMsg:" . error_get_last());
+    die(json_encode(array('type' => 'message', 'text' => 'Tu mensaje no se envió correctamente ' . $user_Name . ', intenta mas tarde o comunicate a ventas@ruffinogroup.com.uy')));
+  }
+} else {
+  die(json_encode(
+                  array(
+                      'type' => 'error',
+                      'text' => 'Todos los campos son obligatorios'
+  )));
 }
-?>
--- js/custom/home.js
+++ js/custom/home.js
@@ -84,3 +84,4 @@
   Home.initialize();
 
 })();
+
