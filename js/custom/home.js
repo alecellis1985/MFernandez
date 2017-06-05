a0a25f8a12467a14b30bc69557ad4b84db35ee01
MSI\Alexander <alecellis1985@gmail.com>
2017-06-05 14:08:45 -0300

    contacto click fixed

--- js/custom/home.js
+++ js/custom/home.js
@@ -1,107 +1,107 @@
 (function () {
 
-  "use strict";
+    "use strict";
 
-  var Home = {
-    initialized: false,
-    initialize: function () {
-      if (this.initialized)
-        return;
-      this.initialized = true;
-      this.build();
-      this.menu();
-      this.carousel();
-    },
-    build: function (options) {
-      // Home Slider#1
-      // Revolution Slider Initialize
-      $(".home-slider-1 .tp-banner").each(function () {
+    var Home = {
+        initialized: false,
+        initialize: function () {
+            if (this.initialized)
+                return;
+            this.initialized = true;
+            this.build();
+            this.menu();
+            this.carousel();
+        },
+        build: function (options) {
+            // Home Slider#1
+            // Revolution Slider Initialize
+            $(".home-slider-1 .tp-banner").each(function () {
 
-        var slider = $(this);
+                var slider = $(this);
 
-        var defaults = {
-          delay: 9000,
-          startheight: 750,
-          startwidth: 960,
+                var defaults = {
+                    delay: 9000,
+                    startheight: 750,
+                    startwidth: 960,
 
-          hideThumbs: 10,
+                    hideThumbs: 10,
 
-          thumbWidth: 100,
-          thumbHeight: 50,
-          thumbAmount: 5,
+                    thumbWidth: 100,
+                    thumbHeight: 50,
+                    thumbAmount: 5,
 
-          navigationType: "both",
-          navigationArrows: "verticalcentered",
-          navigationStyle: "preview1",
+                    navigationType: "both",
+                    navigationArrows: "verticalcentered",
+                    navigationStyle: "preview1",
 
-          touchenabled: "on",
-          onHoverStop: "on",
+                    touchenabled: "on",
+                    onHoverStop: "on",
 
-          navOffsetHorizontal: 0,
-          navOffsetVertical: 20,
+                    navOffsetHorizontal: 0,
+                    navOffsetVertical: 20,
 
-          stopAtSlide: 0,
-          stopAfterLoops: -1,
+                    stopAtSlide: 0,
+                    stopAfterLoops: -1,
 
-          shadow: 0,
-          fullWidth: "on",
-          //forceFullWidth:"off",
-          videoJsPath: "vendor/rs-plugin/videojs/"
+                    shadow: 0,
+                    fullWidth: "on",
+                    //forceFullWidth:"off",
+                    videoJsPath: "vendor/rs-plugin/videojs/"
+                }
+
+                var config = $.extend({}, defaults, options, slider.data("plugin-options"));
+
+                // Initialize Slider
+                var sliderApi = slider.revolution(config).addClass("slider-init");
+
+
+            });
+        },
+
+        menu: function () {
+            $("#myTopnav li a, #jx-main-menu li a,.icons-section a").click(function (e) {
+                var href = $(this).attr('href');
+                if (href.indexOf('http') === -1 && href.indexOf('#') !== -1) {
+                    e.preventDefault();
+                    var $thisBlock = $($(this).attr('href'));
+                    if ($thisBlock) {
+                        $('html, body').animate({
+                            scrollTop: $thisBlock.offset().top
+                        }, 1000);
+                    }
+                    return false;
+                }
+            });
+        },
+        carousel: function () {
+            /*var $item = $('.carousel .item');
+             var $wHeight = $(window).height();
+             $item.eq(0).addClass('active');
+             $item.height($wHeight);
+             $item.addClass('full-screen');*/
+            /*
+             $('.carousel img').each(function () {
+             var $src = $(this).attr('src');
+             var $color = $(this).attr('data-color');
+             $(this).parent().css({
+             'background-image': 'url(' + $src + ')',
+             'background-color': $color
+             });
+             $(this).remove();
+             });*/
+            /*
+             $(window).on('resize', function () {
+             $wHeight = $(window).height();
+             $item.height($wHeight);
+             });*/
+            /*
+             $('.carousel').carousel({
+             interval: 6000,
+             pause: "false"
+             });*/
         }
+    };
 
-        var config = $.extend({}, defaults, options, slider.data("plugin-options"));
-
-        // Initialize Slider
-        var sliderApi = slider.revolution(config).addClass("slider-init");
-
-
-      });
-    },
-
-    menu: function () {
-      $("#myTopnav li a, #jx-main-menu li a,.icons-section a").click(function (e) {
-        var href = $(this).attr('href');
-        if (href.indexOf('#') !== -1) {
-          e.preventDefault();
-          var $thisBlock = $($(this).attr('href'));
-          if ($thisBlock) {
-            $('html, body').animate({
-              scrollTop: $thisBlock.offset().top
-            }, 1000);
-          }
-          return false;
-        }
-      });
-    },
-    carousel: function () {
-      /*var $item = $('.carousel .item');
-       var $wHeight = $(window).height();
-       $item.eq(0).addClass('active');
-       $item.height($wHeight);
-       $item.addClass('full-screen');*/
-      /*
-       $('.carousel img').each(function () {
-       var $src = $(this).attr('src');
-       var $color = $(this).attr('data-color');
-       $(this).parent().css({
-       'background-image': 'url(' + $src + ')',
-       'background-color': $color
-       });
-       $(this).remove();
-       });*/
-      /*
-       $(window).on('resize', function () {
-       $wHeight = $(window).height();
-       $item.height($wHeight);
-       });*/
-      /*
-       $('.carousel').carousel({
-       interval: 6000,
-       pause: "false"
-       });*/
-    }
-  };
-
-  Home.initialize();
+    Home.initialize();
 })();
 
